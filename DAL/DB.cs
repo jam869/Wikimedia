4bb8e521018811bb6ba214570e3a590ad13319d4
jam869 <62719891+jam869@users.noreply.github.com>
2026-03-13 11:07:44 -0400

    Add project files.

---
+++ DAL/DB.cs
@@ -0,0 +1,28 @@
+﻿using EmailHandling;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Hosting;
+using Models; 
+
+
+namespace DAL
+{
+    public sealed class DB
+    {
+        #region singleton setup
+        private static readonly DB instance = new DB();
+        public static DB Instance { get { return instance; } }
+        #endregion
+
+        public static MediasRepository Medias { get; set; } = new MediasRepository();
+
+        public static UsersRepository Users { get; set; } = new UsersRepository();
+        public static LoginsRepository Logins { get; set; } = new LoginsRepository();
+        public static NotificationsRepository Notifications { get; set; } = new NotificationsRepository();
+        public static EventsRepository Events { get; set; } = new EventsRepository();
+
+        public static Repository<UnverifiedEmail> UnverifiedEmails { get; set; } = new Repository<UnverifiedEmail>();
+        public static Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; } = new Repository<RenewPasswordCommand>();
+    }
+}
